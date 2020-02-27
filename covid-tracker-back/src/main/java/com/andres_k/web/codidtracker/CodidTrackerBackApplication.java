ee9bd7db341eb5be2dd903a871ec785e759292e5
kevin.andres <kevin.andres@epitech.eu>
2020-02-27 13:39:04 +0800

    base commit

---
+++ covid-tracker-back/src/main/java/com/andres_k/web/codidtracker/CodidTrackerBackApplication.java
@@ -0,0 +1,13 @@
+package com.andres_k.web.codidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodidTrackerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodidTrackerBackApplication.class, args);
+    }
+
+}
---
+++ covid-tracker-back/src/test/java/com/andres_k/web/codidtracker/CodidTrackerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres_k.web.codidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodidTrackerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
